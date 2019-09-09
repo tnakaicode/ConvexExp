814df513c5ccbcfa897defe718cb1d2c6337c9f2
Taku <tnakaicode@gmail.com>
2019-09-10 00:50:50 +0900

    random split

--- split_box.py
+++ split_box.py
@@ -18,47 +18,25 @@ if __name__ == "__main__":
 
     box = make_box(100, 100, 100)
 
-    p1, v1 = gp_Pnt(50, 50, 50), gp_Vec(0, 0, -1)
-    fc1 = make_face(gp_Pln(p1, vec_to_dir(v1)), -1000, 1000, -
-                    1000, 1000)  # limited, not infinite plane
-
-    p2, v2 = gp_Pnt(50, 50, 50), gp_Vec(0, 1, -1)
-    fc2 = make_face(gp_Pln(p2, vec_to_dir(v2)), -1000, 1000, -
-                    1000, 1000)  # limited, not infinite plane
-
-    p3, v3 = gp_Pnt(50, 50, 25), gp_Vec(1, 1, -1)
-    fc3 = make_face(gp_Pln(p3, vec_to_dir(v3)), -1000, 1000, -
-                    1000, 1000)  # limited, not infinite plane
-
-    bo = BOPAlgo_Builder()
-    bo.AddArgument(box)
-    bo.AddArgument(fc1)
-    bo.AddArgument(fc2)
-    bo.AddArgument(fc3)
-
     splitter = GEOMAlgo_Splitter()
     splitter.AddArgument(box)
-    splitter.AddTool(fc1)
-    splitter.AddTool(fc2)
-    splitter.AddTool(fc3)
+
+    for i in range(5):
+        pnt = gp_Pnt(*np.random.rand(3) * 100)
+        vec = gp_Vec(*np.random.randn(3))
+        pln = gp_Pln(pnt, vec_to_dir(vec))
+        fce = make_face(pln, -1000, 1000, -1000, 1000)
+        splitter.AddTool(fce)
+
     splitter.Perform()
-    #display.DisplayShape(splitter.Shape())
-    
+    # display.DisplayShape(splitter.Shape())
+
     exp = TopExp_Explorer(splitter.Shape(), TopAbs_SOLID)
     shp = []
     while exp.More():
         shp.append(exp.Current())
         exp.Next()
     display.DisplayShape(shp[0])
-
-    """bo.Perform()
-    print("error status: {}".format(bo.ErrorStatus()))
-
-    colos = ["BLUE", "RED", "GREEN", "YELLOW", "BLACK", "WHITE", "BLUE", "RED"]
-
-    top = Topo(bo.Shape())
-    for i, sol in enumerate(top.solids()):
-        display.DisplayShape(sol, color=colos[i], transparency=0.5)"""
 
     display.FitAll()
     start_display()
