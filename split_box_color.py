21bf72fab6c1b344566cfd88cabb38adb1468c76
Taku <tnakaicode@gmail.com>
2019-09-09 23:15:45 +0900

    test push

--- split_box_color.py
+++ split_box_color.py
@@ -14,8 +14,6 @@ from OCCUtils.Topology import Topo
 from OCCUtils.Construct import make_box, make_face
 from OCCUtils.Construct import vec_to_dir
 
-colos = ["BLUE", "RED", "GREEN", "YELLOW", "BLACK", "WHITE"]
-print(colos)
 
 if __name__ == "__main__":
     display, start_display, add_menu, add_function_to_menu = init_display()
@@ -34,6 +32,7 @@ if __name__ == "__main__":
 
     bo.Perform()
     print("error status: {}".format(bo.ErrorStatus()))
+    colos = ["BLUE", "RED", "GREEN", "YELLOW", "BLACK", "WHITE"]
 
     top = Topo(bo.Shape())
     for i, sol in enumerate(top.solids()):
