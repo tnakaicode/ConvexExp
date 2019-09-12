0e95b90c2727c97e13772d4002bb2564976c60b9
Taku Nakai WSL <nakai.taku@qst.go.jp>
2019-09-12 17:52:25 +0900

    class

--- split_box_prop.py
+++ split_box_prop.py
@@ -1,8 +1,9 @@
 import numpy as np
 
 from OCC.Display.SimpleGui import init_display
+from OCC.Core.gp import gp_Pnt, gp_Vec, gp_Dir
+from OCC.Core.gp import gp_Ax3
 from OCC.Core.gp import gp_Pln
-from OCC.Core.gp import gp_Pnt, gp_Vec
 from OCC.Core.TopoDS import TopoDS_Compound, TopoDS_Solid, TopoDS_Shape
 from OCC.Core.BRep import BRep_Builder, BRep_Tool
 from OCC.Core.BRepCheck import BRepCheck_Analyzer
@@ -18,47 +19,51 @@ from OCC.Extend.DataExchange import write_step_file, write_stl_file
 from OCC.Extend.ShapeFactory import make_box, make_face
 from OCCUtils.Construct import vec_to_dir
 
+
+class CovExp (object):
+
+    def __init__(self):
+        self.prop = GProp_GProps()
+        self.base = make_box(100, 100, 100)
+        self.base_vol = self.cal_vol(self.base)
+
+        self.splitter = GEOMAlgo_Splitter()
+        self.splitter.AddArgument(self.base)
+        print(self.cal_vol(self.base))
+
+    def split_run(self, num=5):
+        for i in range(num):
+            pnt = gp_Pnt(*np.random.rand(3) * 100)
+            vec = gp_Vec(*np.random.randn(3))
+            pln = gp_Pln(pnt, vec_to_dir(vec))
+            fce = make_face(pln, -1000, 1000, -1000, 1000)
+            self.splitter.AddTool(fce)
+        self.splitter.Perform()
+
+    def cal_vol(self, shp=TopoDS_Shape()):
+        brepgprop_VolumeProperties(shp, self.prop)
+        return self.prop.Mass()
+
+    def prop_solids(self):
+        self.exp = TopExp_Explorer(self.splitter.Shape(), TopAbs_SOLID)
+        while self.exp.More():
+            print(self.cal_vol(self.exp.Current()), self.base_vol)
+
+            shp_exp = TopExp_Explorer(self.exp.Current(), TopAbs_FACE)
+            fce = shp_exp.Current()
+            cheker = BRepCheck_Analyzer(fce)
+            """shp_exp.Next()
+            while shp_exp.More():
+                fc1 = shp_exp.Current()
+                print(shp_exp.Depth())
+                print(shp_exp.Depth(), cheker.IsValid(fc1))
+                shp_exp.Next()"""
+            self.exp.Next()
+
+
 if __name__ == "__main__":
-    num = 0
-    props = GProp_GProps()
-    box = make_box(100, 100, 100)
+    obj = CovExp()
+    obj.split_run()
+    obj.prop_solids()
 
-    vol = brepgprop_VolumeProperties(box, props)
-    print(vol)
-
-    splitter = GEOMAlgo_Splitter()
-    splitter.AddArgument(box)
-
-    for i in range(5):
-        pnt = gp_Pnt(*np.random.rand(3) * 100)
-        vec = gp_Vec(*np.random.randn(3))
-        pln = gp_Pln(pnt, vec_to_dir(vec))
-        fce = make_face(pln, -1000, 1000, -1000, 1000)
-        splitter.AddTool(fce)
-
-    splitter.Perform()
-
-    exp = TopExp_Explorer(splitter.Shape(), TopAbs_SOLID)
-    shp = []
-    vol = 0
-    while exp.More():
-        props = GProp_GProps()
-        num += 1
-
-        cov = exp.Current()
-        shp.append(cov)
-
-        shp_exp = TopExp_Explorer(cov, TopAbs_FACE)
-        fce = shp_exp.Current()
-        cheker = BRepCheck_Analyzer(fce)
-        shp_exp.Next()
-        while shp_exp.More():
-            fc1 = shp_exp.Current()
-            print(shp_exp.Depth())
-            print(shp_exp.Depth(), cheker.IsValid(fc1))
-            shp_exp.Next()
-
-        brepgprop_VolumeProperties(cov, props)
-        vol += props.Mass()
-        print(props.Mass(), vol)
-        exp.Next()
+    print(obj.cal_vol())
