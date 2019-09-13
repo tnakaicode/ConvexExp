0c241a2709cd8598ebb9db5f6d1a7a7e5600fdcc
Taku <tnakaicode@gmail.com>
2019-09-13 21:42:23 +0900

    face expand

--- split_box_prop.py
+++ split_box_prop.py
@@ -5,13 +5,13 @@ from OCC.Core.gp import gp_Pnt, gp_Vec, gp_Dir
 from OCC.Core.gp import gp_Ax3
 from OCC.Core.gp import gp_Pln
 from OCC.Core.BRep import BRep_Builder, BRep_Tool
-from OCC.Core.BRepGProp import brepgprop_SurfaceProperties, brepgprop_VolumeProperties
+from OCC.Core.BRepGProp import brepgprop_SurfaceProperties, brepgprop_VolumeProperties, brepgprop_LinearProperties
 from OCC.Core.BRepCheck import BRepCheck_Analyzer
 from OCC.Core.BRepAlgo import BRepAlgo_BooleanOperation
 from OCC.Core.BOPAlgo import BOPAlgo_MakerVolume, BOPAlgo_Builder
 from OCC.Core.LocOpe import LocOpe_FindEdges
 from OCC.Core.TopExp import TopExp_Explorer
-from OCC.Core.TopoDS import TopoDS_Compound, TopoDS_Solid, TopoDS_Shape
+from OCC.Core.TopoDS import TopoDS_Compound, TopoDS_Solid, TopoDS_Shape, TopoDS_Face
 from OCC.Core.TopAbs import TopAbs_EDGE, TopAbs_SHAPE, TopAbs_SOLID, TopAbs_FACE
 from OCC.Core.TopTools import TopTools_ListOfShape
 from OCC.Core.TCollection import TCollection_ExtendedString_IsEqual
@@ -25,14 +25,26 @@ from OCCUtils.Construct import vec_to_dir
 
 class CovExp (object):
 
-    def __init__(self):
+    def __init__(self, file=False):
         self.prop = GProp_GProps()
-        self.base = make_box(100, 100, 100)
+        self.base = make_box(100, 200, 300)
         self.base_vol = self.cal_vol(self.base)
 
         self.splitter = GEOMAlgo_Splitter()
         self.splitter.AddArgument(self.base)
         print(self.cal_vol(self.base))
+
+    def fileout(self):
+        num = 0
+        stp_file = "./shp/shp_{:04d}.stp".format(num)
+        write_step_file(self.base, stp_file)
+
+        sol_exp = TopExp_Explorer(self.splitter.Shape(), TopAbs_SOLID)
+        while sol_exp.More():
+            num += 1
+            stp_file = "./shp/shp_{:04d}.stp".format(num)
+            write_step_file(sol_exp.Current(), stp_file)
+            sol_exp.Next()
 
     def split_run(self, num=5):
         for i in range(num):
@@ -51,24 +63,31 @@ class CovExp (object):
         brepgprop_SurfaceProperties(shp, self.prop)
         return self.prop.Mass()
 
+    def cal_len(self, shp=TopoDS_Shape()):
+        brepgprop_LinearProperties(shp, self.prop)
+        return self.prop.Mass()
+
+    def face_expand(self, face=TopoDS_Face()):
+        print(self.cal_are(face))
+
+        find_edge = LocOpe_FindEdges(self.tmp_face, face)
+        find_edge.InitIterator()
+        while find_edge.More():
+            edge = find_edge.EdgeFrom()
+            print(self.cal_len(edge), self.cal_are(face))
+            find_edge.Next()
+
     def prop_soild(self, sol=TopoDS_Solid()):
         sol_exp = TopExp_Explorer(sol, TopAbs_FACE)
         sol_top = TopologyExplorer(sol)
         print(self.cal_vol(sol), self.base_vol)
         print(sol_top.number_of_faces())
 
-        fc0 = sol_exp.Current()
+        self.tmp_face = sol_exp.Current()
         sol_exp.Next()
 
         while sol_exp.More():
-            fc1 = sol_exp.Current()
-            print(self.cal_are(fc0), self.cal_are(fc1))
-
-            find_edge = LocOpe_FindEdges(fc0, fc1)
-            find_edge.InitIterator()
-            while find_edge.More():
-                print(find_edge.EdgeFrom())
-                find_edge.Next()
+            self.face_expand(sol_exp.Current())
             sol_exp.Next()
 
     def prop_solids(self):
@@ -84,3 +103,5 @@ if __name__ == "__main__":
     obj.prop_solids()
 
     print(obj.cal_vol())
+    obj.prop_soild(obj.base)
+    obj.fileout()
