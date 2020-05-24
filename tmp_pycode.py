edce31afa1f4673051fc35fbf4a59f2ed1850dcc
TakuNakai <nakai.taku@outlook.jp>
2020-05-24 09:55:43 +0900

    add base

---
+++ tmp_pycode.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import os
+import time
+from optparse import OptionParser
+
+sys.path.append(os.path.join("./"))
+from base import plot2d
+
+import logging
+logging.getLogger('matplotlib').setLevel(logging.ERROR)
+
+from OCC.Core.gp import gp_Pnt, gp_Vec, gp_Dir
+from OCC.Core.gp import gp_Ax1, gp_Ax2, gp_Ax3
+from OCC.Core.gp import gp_Pln, gp_Lin
+from OCC.Core.gp import gp_Trsf
+from OCC.Core.TopLoc import TopLoc_Location
+from OCC.Core.BRepLProp import BRepLProp_CLProps
+from OCC.Core.BRepGProp import brepgprop_SurfaceProperties
+from OCC.Core.BRepGProp import brepgprop_VolumeProperties
+from OCC.Core.BRepGProp import brepgprop_LinearProperties
+from OCC.Core.BOPAlgo import BOPAlgo_Splitter
+from OCCUtils.Construct import vec_to_dir, dir_to_vec
+from OCCUtils.Construct import make_box
+
+if __name__ == '__main__':
+    argvs = sys.argv
+    parser = OptionParser()
+    parser.add_option("--dir", dest="dir", default="./")
+    parser.add_option("--pxyz", dest="pxyz",
+                      default=[0.0, 0.0, 0.0], type="float", nargs=3)
+    opt, argc = parser.parse_args(argvs)
+    print(opt, argc)
